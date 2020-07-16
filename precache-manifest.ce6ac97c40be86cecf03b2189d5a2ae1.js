e4c349574ef58f4f65c58d02dfce5cab8100292f
Patrick Santos <patrickspteixeira@gmail.com>
2020-07-16 16:09:16 -0300

    Updates

---
+++ precache-manifest.ce6ac97c40be86cecf03b2189d5a2ae1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88389e3d6ff1f020e576458a6ebc92d5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "68e686ba5a3c8a906cde",
+    "url": "./static/css/main.d4ad9e16.chunk.css"
+  },
+  {
+    "revision": "016616b45491d5552836",
+    "url": "./static/js/2.8be8bb1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8be8bb1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68e686ba5a3c8a906cde",
+    "url": "./static/js/main.90d8a112.chunk.js"
+  },
+  {
+    "revision": "6da796fdc5062a7a3942",
+    "url": "./static/js/runtime-main.c7e3870d.js"
+  },
+  {
+    "revision": "b195082a5346e6d321d450c5c7a3bcce",
+    "url": "./static/media/close~yJuNWzHg.b195082a.svg"
+  },
+  {
+    "revision": "3cf791069d29cb56f0fd7484ae62d361",
+    "url": "./static/media/trash~XOtNinTH.3cf79106.svg"
+  }
+]);
